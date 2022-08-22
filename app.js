3d7cd366817c8a13e0eb143c4da5204066c26442
MelissaHub <Melissadevelopment2022@gmail.com>
2022-08-21 21:21:39 -0400

    list attempts

--- app.js
+++ app.js
@@ -1,49 +1,11 @@
-const receipts = [
-    {
-      person: 'Karolin',
-      order: {
-        main: 'Burrito',
-        protein: 'Organic Tofu',
-        rice: 'Purple Rice',
-        sauce: 'Green Crack',
-        toppings: [
-          'Baby Bok Choy', 'Cucumber Kimchi'
-        ],
-        drink: 'Korchata',
-        cost: 22
-      },
-      paid: false
-    },
-    {
-      person: 'Mark',
-      order: {
-        main: 'Rice Bowl',
-        protein: 'Ginger Soy Chix',
-        rice: 'Sticky Rice',
-        sauce: 'Korilla',
-        toppings: [
-          'Yuzu Pickled Sweet Pepper', 'Kale'
-        ],
-        drink: 'Korchata',
-        cost: 19
-      },
-      paid: false
-    },
-    {
-      person: 'Matt',
-      order: {
-        main: 'Salad Bowl',
-        protein: 'Organic Tofu',
-        rice: 'none',
-        sauce: "K'lla",
-        toppings: [
-          'Blue Potato Salad', 'Pico De Gallo', 'Red Kimchi'
-        ],
-        drink: 'Sparkling Blood Orange Soda',
-        cost: 20
-      },
-      paid: true
-    }
+const list = [
+  {
+    item: '',
+    brand: '',
+    units: '',
+    quantity: 0,
+    isPurchased: false
+  }
   ]
   
   
@@ -52,30 +14,30 @@ const receipts = [
         super()
       }
     
-      state = { receipts: receipts }
+      state = { list: list }
       render() {
           return (
             <div>
               <h1>test</h1>
-              {this.state.receipts.map(receipt => {
+              {this.state.list.map(list => {
                 return (
                   <div>
-                    <h3>{receipt.person}</h3>
+                    <h3>List</h3>
                     <ul>
-                      <li>Main: {receipt.order.main}</li>
-                      <li>Protein: {receipt.order.protein}</li>
-                      <li>Rice: {receipt.order.rice}</li>
-                      <li>Sauce: {receipt.order.sauce}</li>
+                      <li>Item: {list.state.main}</li>
+                      <li>Main: {list.state.protein}</li>
+                      <li>Units: {list.state.rice}</li>
+                     
                       <li>
-                        Toppings:
+                        Quantity:
                         <ul>
-                          {receipt.order.toppings.map(topping => {
-                            return <li>{topping}</li>
+                          {receiptstate.qunatity.map(qunatity=> {
+                            return <li>{qunatity}</li>
                           })}
                         </ul>
                       </li>
-                      <li>Drink: {receipt.order.drink}</li>
-                      <li>Cost: {receipt.order.cost}</li>
+                      <li>Is Purchased {list.state.drink}</li>
+
                     </ul>
                   </div>
                 )
